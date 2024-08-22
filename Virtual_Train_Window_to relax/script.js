5c773bd7564c71004ca6d426fe80be0912917dfd
higuson082 <91842949+higuson082@users.noreply.github.com>
2024-08-23 04:08:15 +0900

    Add files via upload

---
+++ Virtual_Train_Window_to relax/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const backgroundMusic = document.getElementById('background-music');
+    backgroundMusic.volume = 0.2;
+    backgroundMusic.play();
+
+    console.log('The calming sky is ready to soothe your mind.');
+});
